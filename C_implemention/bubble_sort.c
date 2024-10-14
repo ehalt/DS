1e17afea2976d51e0ed8e0c200dc7610f2893367
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2024-10-14 15:47:50 +0600

    bubble sort

---
+++ C_implemention/bubble_sort.c
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+
+void bubble_sort(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void print_arr(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    bubble_sort(arr, n);
+
+    print_arr(arr, n);
+
+    return 0;
+}
